8bada9b6832ab4df5eb3e90228aca016e7399456
Kayvan Shah <kayvan.shah@usc.edu>
2023-09-03 15:27:42 -0700

    sample tests

---
+++ tests/test_sample.py
@@ -0,0 +1,52 @@
+import pytest
+
+from lab2 import check_perfect_squares, format_date, split_by_delimiter
+
+
+@pytest.mark.parametrize(
+    "input_args, expected_result, exception_message",
+    [
+        ({"day": 1, "month": 8, "year": 2000}, "01 August, 2000", None),
+        ({"day": 30, "month": 13, "year": 2003}, None, "The given date: 30, 13, 2003 is invalid"),
+    ],
+)
+def test_format_date(input_args, expected_result, exception_message):
+    if expected_result is not None:
+        result = format_date(**input_args)
+        assert result == expected_result
+    if exception_message is not None:
+        with pytest.raises(Exception) as e_info:
+            assert str(e_info.value) == exception_message
+
+
+@pytest.mark.parametrize(
+    "input_args, expected_result, exception_message",
+    [
+        ({"some_string": 12, "delimiter": " "}, None, "Invalid Input"),
+        ({"some_string": "1-2-3-4-5", "delimiter": "-"}, ["1", "2", "3", "4", "5"], None),
+    ],
+)
+def test_split_by_delimiter(input_args, expected_result, exception_message):
+    if expected_result is not None:
+        result = split_by_delimiter(**input_args)
+        assert result == expected_result
+    if exception_message is not None:
+        with pytest.raises(Exception) as e_info:
+            assert str(e_info.value) == exception_message
+
+
+@pytest.mark.parametrize(
+    "input_args, expected_result, exception_message",
+    [
+        ({"tups": [(1, 1), (2, 4), (3, 9), (4, 15)]}, False, None),
+        ({"tups": [("1, 1", (2, 4), (3, 9), (4, 16), (5, 25), (6, 36))]}, None, "Invalid Input"),
+        ({"tups": [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]}, True, None),
+    ],
+)
+def test_check_perfect_squares(input_args, expected_result, exception_message):
+    if expected_result is not None:
+        result = check_perfect_squares(**input_args)
+        assert result == expected_result
+    if exception_message is not None:
+        with pytest.raises(Exception) as e_info:
+            assert str(e_info.value) == exception_message
