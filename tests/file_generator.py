44c0d442f6037a7c3f6de5fdb8aa46906bcfe497
Kayvan Shah <kayvan.shah@usc.edu>
2023-09-03 20:21:30 -0700

    modularization of script

--- tests/file_generator.py
+++ tests/file_generator.py
@@ -1,101 +1,223 @@
 import os
 import json
 import subprocess
+import multiprocessing
 
 
-def serialize_tuples(obj):
-    if isinstance(obj, tuple):
-        return {"__tuple__": True, "items": obj}
-    if isinstance(obj, list):
-        return [serialize_tuples(item) for item in obj]
-    if isinstance(obj, dict):
-        return {key: serialize_tuples(value) for key, value in obj.items()}
-    return obj
+class SerializerDeserializer:
+    """
+    A utility class for serializing and deserializing objects.
+    """
+
+    @staticmethod
+    def serialize_tuples(obj):
+        """Serialize tuples within an object to a dictionary format."""
+        if isinstance(obj, tuple):
+            return {"__tuple__": True, "items": obj}
+        if isinstance(obj, list):
+            return [SerializerDeserializer.serialize_tuples(item) for item in obj]
+        if isinstance(obj, dict):
+            return {
+                key: SerializerDeserializer.serialize_tuples(value) for key, value in obj.items()
+            }
+        return obj
+
+    @staticmethod
+    def serialize_string(obj):
+        """Serialize a string by adding double quotes around it."""
+        if isinstance(obj, str):
+            return f'"{obj}"'
+        return obj
+
+    @staticmethod
+    def deserialize_tuples(obj):
+        """Deserialize a serialized object back into tuples."""
+        if isinstance(obj, list):
+            return [SerializerDeserializer.deserialize_tuples(item) for item in obj]
+        if isinstance(obj, dict):
+            if "__tuple__" in obj:
+                # Check if the items in the tuple are themselves dictionaries
+                items = obj["items"]
+                deserialized_items = [
+                    SerializerDeserializer.deserialize_tuples(item)
+                    if isinstance(item, dict)
+                    else item
+                    for item in items
+                ]
+                return tuple(deserialized_items)
+            return {
+                key: SerializerDeserializer.deserialize_tuples(value) for key, value in obj.items()
+            }
+        return obj
 
 
-def serialize_string(obj):
-    if isinstance(obj, str):
-        return f'"{obj}"'
-    return obj
+class TestFileGenerator:
+    def __init__(self, config_path, output_dir, template):
+        """
+        Initialize a TestFileGenerator instance.
 
+        Args:
+            config_path (str): Path to the JSON configuration file.
+            output_dir (str): Directory where test files will be generated.
+            template (str): Template for generating test files.
+        """
+        self.config_path = config_path
+        self.output_dir = output_dir
+        self.template = template
 
-def deserialize_tuples(obj):
-    if isinstance(obj, list):
-        return [deserialize_tuples(item) for item in obj]
-    if isinstance(obj, dict):
-        if "__tuple__" in obj:
-            # Check if the items in the tuple are themselves dictionaries
-            items = obj["items"]
-            deserialized_items = [
-                deserialize_tuples(item) if isinstance(item, dict) else item for item in items
-            ]
-            return tuple(deserialized_items)
-        return {key: deserialize_tuples(value) for key, value in obj.items()}
-    return obj
+    def create_output_directory(self, output_dir):
+        """Create the output directory if it doesn't exist."""
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
+    def read_config(self):
+        """Read and deserialize the JSON configuration file."""
+        with open(self.config_path, "r") as f:
+            return SerializerDeserializer.deserialize_tuples(json.load(f))
 
-template = """import pytest
+    def generate_test_case_content(self, function_name, function_args_name, test_case):
+        """
+        Generate the content for a test case file.
 
-from lab2 import {function_name}
+        Args:
+            function_name (str): Name of the test function.
+            function_args_name (list): Names of function arguments.
+            test_case (dict): Test case configuration.
 
-
-@pytest.mark.parametrize(
-    'input_args, expected_result, exception_message',
-    [
-        ({test_cases}, {expected_result}, {exception_message})
-    ]
-)
-def test_{function_name}(input_args, expected_result, exception_message):
-    if exception_message is not None:
-        with pytest.raises(Exception) as e_info:
-            {function_name}(**input_args)
-        assert str(e_info.value) == exception_message
-    else:
-        result = {function_name}(**input_args)
-        assert result == expected_result
-"""
-
-DIR_PATH = os.path.dirname(__file__)
-TEST_CONFIG_PATH = os.path.join(DIR_PATH, "config.json")
-
-with open(TEST_CONFIG_PATH, "r") as f:
-    config = deserialize_tuples(json.load(f))
-
-
-# Loop through each test function in the configuration
-for test_function_config in config["tests"]:
-    function_name = test_function_config["test_function"]
-    function_args_name = test_function_config["args_name"]
-
-    # Define the output directory where you want to generate the test files
-    OUTPUT_DIR = os.path.join(DIR_PATH, f"test_{function_name}")
-    INIT_FILE_PATH = os.path.join(OUTPUT_DIR, "__init__.py")
-
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(OUTPUT_DIR):
-        os.makedirs(OUTPUT_DIR)
-
-    if not os.path.exists(INIT_FILE_PATH):
-        with open(INIT_FILE_PATH, "w") as init_file:
-            pass
-
-    test_cases = test_function_config["test_cases"]
-
-    for test_case in test_cases:
-        # Generate the test file content using the template
-        test_file_content = template.format(
+        Returns:
+            str: Content of the test case file.
+        """
+        return self.template.format(
             function_name=function_name,
             test_cases=dict(zip(function_args_name, test_case["args"])),
-            expected_result=serialize_string(test_case["expected_result"]),
-            exception_message=serialize_string(test_case["exception_message"]),
+            expected_result=SerializerDeserializer.serialize_string(test_case["expected_result"]),
+            exception_message=SerializerDeserializer.serialize_string(
+                test_case["exception_message"]
+            ),
         )
 
-        # Write the test file content to a Python file
-        TEST_CASE_FILE_NAME = test_case["name"]
-        test_file_path = os.path.join(OUTPUT_DIR, f"{TEST_CASE_FILE_NAME}.py")
+    def write_test_case_file(self, output_dir, test_case_file_name, test_case_content):
+        """
+        Write the content of a test case file to disk.
 
+        Args:
+            output_dir (str): Directory where the test case file will be saved.
+            test_case_file_name (str): Name of the test case file.
+            test_case_content (str): Content of the test case file.
+        """
+        test_file_path = os.path.join(output_dir, f"{test_case_file_name}.py")
         with open(test_file_path, "w") as test_file:
-            test_file.write(test_file_content)
+            test_file.write(test_case_content)
 
-    # Format the generated test file using black
-    subprocess.run(["black", OUTPUT_DIR, "--preview", "--line-length=100", "--workers=2"])
+    def format_test_case_files(self, output_dir):
+        """Format the generated test case files using the black formatter."""
+        num_workers = multiprocessing.cpu_count()
+        print(f"Number of CPU cores available: {num_workers}")
+
+        subprocess.run(
+            ["black", output_dir, "--preview", "--line-length=100", f"--workers={num_workers}"]
+        )
+
+    def generate_test_files(self):
+        """Generate test case files based on the configuration and format them."""
+        config = self.read_config()
+
+        for test_function_config in config["tests"]:
+            function_name = test_function_config["test_function"]
+            function_args_name = test_function_config["args_name"]
+
+            # Define the output directory where you want to generate the test files
+            output_dir = os.path.join(self.output_dir, f"test_{function_name}")
+            self.create_output_directory(output_dir)
+
+            # Create the __init__.py file for the test functions colleactions
+            init_file_path = os.path.join(output_dir, "__init__.py")
+            if not os.path.exists(init_file_path):
+                with open(init_file_path, "w"):
+                    pass
+
+            for test_case in test_function_config["test_cases"]:
+                test_case_file_name = test_case["name"]
+                test_case_content = self.generate_test_case_content(
+                    function_name, function_args_name, test_case
+                )
+
+                self.write_test_case_file(output_dir, test_case_file_name, test_case_content)
+
+            self.format_test_case_files(output_dir)
+
+
+class AutogradingConfigGenerator:
+    def __init__(self, test_config_file, output_config_file):
+        self.test_config_file = test_config_file
+        self.output_config_file = output_config_file
+
+    def read_config(self):
+        """Read and deserialize the JSON configuration file."""
+        with open(self.test_config_file, "r") as f:
+            return json.load(f)
+
+    def write_config(self, config):
+        """Write the generated configuration to the output file."""
+        with open(self.output_config_file, "w") as f:
+            json.dump(config, f, indent=2)
+
+    def generate_config(self):
+        test_config = self.read_config()
+        test_cases = []
+
+        for test_function_config in test_config["tests"]:
+            function_name = test_function_config["test_function"]
+            test_suite_dir = f"test_{function_name}"
+            setup_command = "sudo -H pip3 install pytest"
+
+            for test_case in test_function_config["test_cases"]:
+                test_file = test_case["name"]
+                test_cases.append(
+                    {
+                        "name": test_file,
+                        "setup": setup_command,
+                        "run": f"pytest tests/{test_suite_dir}/{test_file}.py",
+                        "timeout": 5,
+                        "points": 1,
+                    }
+                )
+
+        config = {"tests": test_cases}
+        self.write_config(config)
+
+
+if __name__ == "__main__":
+    tests_directory = os.path.dirname(__file__)
+    root_directory = os.path.dirname(tests_directory)
+    test_config_path = os.path.join(tests_directory, "config.json")
+
+    test_template = """import pytest
+
+    from lab2 import {function_name}
+
+
+    @pytest.mark.parametrize(
+        'input_args, expected_result, exception_message',
+        [
+            ({test_cases}, {expected_result}, {exception_message})
+        ]
+    )
+    def test_{function_name}(input_args, expected_result, exception_message):
+        if exception_message is not None:
+            with pytest.raises(Exception) as e_info:
+                {function_name}(**input_args)
+            assert str(e_info.value) == exception_message
+        else:
+            result = {function_name}(**input_args)
+            assert result == expected_result
+    """
+
+    test_file_generator = TestFileGenerator(test_config_path, tests_directory, test_template)
+    test_file_generator.generate_test_files()
+
+    # Output autograding config file
+    output_config_file = os.path.join(root_directory, ".github", "classroom", "autograding.json")
+
+    generator = AutogradingConfigGenerator(test_config_path, output_config_file)
+    generator.generate_config()
